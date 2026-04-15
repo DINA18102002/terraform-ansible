c23ad0c0d14ac5b93242d5f9fc3c8540209906e3
DINA18102002 <theenaxz@gmail.com>
2026-04-15 04:28:49 +0000

    add monitoring

--- fullstackapp/backend/src/app.js
+++ fullstackapp/backend/src/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+// prometheus client
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const register = client.register;
+
 const noteRoutes = require('./routes/noteRoutes');
 
 const app = express();
@@ -9,6 +16,12 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
 
 // Routes
 app.use('/notes', noteRoutes);
